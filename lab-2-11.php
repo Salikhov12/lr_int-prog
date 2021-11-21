84cf9f0ef0617f21a4354388857554caf0aa206d
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2021-11-22 00:56:07 +0500

    Выполнена задача 1-3

---
+++ lab-2-11.php
@@ -0,0 +1,29 @@
+<head>
+<title>Салихов Рашит</title>
+</head>
+<?php
+$N = rand(2,100); #Единицу брать не будем
+$M = rand(101,200);
+$result = 0;
+print('$N = '.$N.', $M = '.$M);
+print("<br><br>Вариант 3.<br>");
+for ($i=2;$i<$N;$i++){
+	if ($N%$i==0){
+		$result +=$i;
+	}
+}
+print ("Характер: ".$result);
+$result = 0;
+print("<br><br>Вариант 6.<br>");
+for ($i=$N;$i<=$M;$i++){
+	$sqr = pow($i,(1.0/3)); #Корень числа третьей степени, для уменьшения количества переборов
+	settype($sqr,int);
+	for ($j=1;$j<=$sqr;$j++){
+		for ($k=$j;$k<=$sqr;$k++){
+			if ( (pow($j,3) + pow($k,3)) == $i){
+				print($i." ");
+			}
+		}
+	}
+}
+?>
