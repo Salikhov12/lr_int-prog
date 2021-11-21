058f38bd48b0028bc498768e2d9911cc65b60af6
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2021-11-21 22:52:07 +0500

    Добавил упражнения и часть 1-ой задачи

---
+++ index.php
@@ -0,0 +1,15 @@
+<p> Дата и время:
+<p>
+<?php
+ 	$d=date("d.m.Y H:i");
+ 	echo($d);
+?>
+<ul>
+<li><a href="lab-1-1.php">1</a>
+<li><a href="lab-1-2.php">2</a>
+<li><a href="lab-1-3.php">3</a>
+<ul>
+<li><a href="lab-1-5.php">5</a>
+</ul>
+
+</ul>
