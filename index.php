411c64f885d9c816a0fd712e697bf0710370162b
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2021-11-21 23:34:33 +0500

    Добавлены задания с 1-6 по 1-9

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
  	$d=date("d.m.Y H:i");
  	echo($d);
 ?>
+
 <ul>
 <li><a href="lab-1-1.php">1</a>
 <li><a href="lab-1-2.php">2</a>
 <li><a href="lab-1-3.php">3</a>
 <ul>
 <li><a href="lab-1-5.php">5</a>
+<li><a href="lab-2-1.php">6</a>
+<li><a href="lab-2-2.php">7</a>
+<li><a href="lab-2-3.php">8</a>
+<li><a href="lab-2-4.php">9</a>
+<li><a href="lab-2-5.php">10</a>
+<li><a href="lab-2-6.php">11</a>
 </ul>
 
 </ul>
---
+++ lab-2-1.php
@@ -0,0 +1,10 @@
+<head>
+<title>Салихов Рашит</title>
+</head>
+<?php
+$a=-1; $b = "";
+if ($a) { echo '$a='."$a - истина<br>"; }
+ else { echo '$a='."$a - ложь<br>"; }
+if ($b) { echo '$b='."$b - истина<br>"; }
+ else { echo '$b='."$b - ложь<br>"; }
+?>
