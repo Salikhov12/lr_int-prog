722bfaedc6081b170cc1f6b951d0702682970e17
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2021-11-23 14:43:32 +0500

    Добавление задания 3-2

---
+++ s3-2.php
@@ -0,0 +1,37 @@
+<HTML>
+<HEAD> <TITLE>Салихов Рашит </TITLE> </HEAD>
+<BODY>
+<FORM method="post" action="<?php print $PHP_SELF ?>">
+ Калькулятор:
+ <INPUT type="number" name="a" required>
+ <INPUT type="number" name="b"  required>
+ <p>
+ <SELECT NAME="z" SIZE="1">
+ <OPTION VALUE="1" SELECTED> сложить
+ <OPTION VALUE="2"> вычесть
+ <OPTION VALUE="3"> умножить
+ <OPTION VALUE="4"> разделить 
+ </SELECT>
+ <P> <INPUT type="submit" name="obr" value="Посчитать">
+</FORM>
+<?
+if (isset($_POST["obr"])) {
+	switch($_POST['z']){
+	 case 1:{
+		 echo ($_POST['a']." + ".$_POST['b']." = ".($_POST['a']+$_POST['b'])); break;
+	 }
+	 case 2:{
+		 echo ($_POST['a']." - ".$_POST['b']." = ".($_POST['a']-$_POST['b'])); break;
+	 }
+	 case 3:{
+		 echo ($_POST['a']." * ".$_POST['b']." = ".($_POST['a']*$_POST['b'])); break;
+	 }
+	 case 4:{
+		 if ($_POST['b']!=0){
+		 echo ($_POST['a']." / ".$_POST['b']." = ".($_POST['a']/$_POST['b'])); break;}
+		 else {echo "Деление на ноль."; break;}
+	 }
+	}
+ }
+?>
+</BODY> </HTML>
