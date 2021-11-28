4dbf0f188c8be25a534c5e4b2b4a6be20e28c4e3
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2021-11-28 16:52:20 +0500

    Исправление ошибки в задании 3-6

--- s3-6.php
+++ s3-6.php
@@ -37,16 +37,16 @@ function vr19($s){
 	echo ("<br><br>Вариант 19<br><br>");
 	$v=0;
 	$n=0;
-	for ($i=0;$i<mb_strlen($s,'utf-8');$i++){
-		$str = mb_substr($s,$i,1,'utf-8');
-		if (preg_match('~^([А-ЯЁ]|[A-Z])~u', $str)==1){
+	for ($i=0;$i<mb_strlen($s,"utf-8");$i++){
+		$str = mb_substr($s,$i,1,"utf-8");
+		if (preg_match("~^([А-ЯЁ]|[A-Z])~u", $str)==1){
 			$v++;
 		}
-		if (preg_match('~^[а-яё]|[a-z]~u', $str)==1){
+		if (preg_match("~^[а-яё]|[a-z]~u", $str)==1){
 			$n++;
 		}
 	}
-	echo ("Строчных букв:".round(($n/mb_strlen($s,'utf-8')*100),1)."%");
-	echo ("<br>Прописных букв:".round(($v/mb_strlen($s,'utf-8')*100),1)."%");
+	echo ("Строчных букв:".round(($n/mb_strlen($s,"utf-8")*100),1)."%");
+	echo ("<br>Прописных букв:".round(($v/mb_strlen($s,"utf-8")*100),1)."%");
 }
 ?>
