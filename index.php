021d95aa49a56d6bbaea4688b2f5eb200777592e
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2022-03-15 00:16:56 +0500

    Выполнение первых двух заданий первой лабораторной работы

---
+++ index.php
@@ -0,0 +1,9 @@
+<head>
+<title>Салихов Рашит</title>
+</head>
+<br>
+<ul>
+Лабораторная работа №1
+<li><a href="Lab1.html">1</a>
+<li><a href="Lab2.html">2</a>
+</ul>
---
+++ main.js
@@ -0,0 +1,3 @@
+var string = "Приветсвую вас!";
+alert(string);
+document.write(string); 
