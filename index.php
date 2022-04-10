f9759b2ba513fcb8ab372fa88f07f87221674892
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2022-04-10 19:52:21 +0500

    Добавление примеров 2 Лр, первых 3-х заданий

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <head>
 <title>Салихов Рашит</title>
 </head>
-<br>
 <ul>
 Лабораторная работа №1
 <li><a href="Lab1.html">1</a>
@@ -11,5 +10,8 @@
 <li><a href="Lab5.html">5</a>
 <li><a href="Lab6.html">6</a>
 <li><a href="Lab7.html">7</a>
-
+<br>Лабораторная работа №2
+<li><a href="lab_2-1.html">1</a>
+<li><a href="lab_2-2.html">2</a>
+<li><a href="lab_2-3.html">3</a>
 </ul>
