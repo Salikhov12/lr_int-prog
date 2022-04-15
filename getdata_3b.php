3391cf53dfab4e42fba9cb1aaf19489824d8b9c7
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2022-04-15 19:23:15 +0500

    Добавление кода для отправки данных в БД, написан код для получения данных из БД

--- getdata_3b.php
+++ getdata_3b.php
@@ -1,21 +1,12 @@
-<html>
-<head> <title> Салихов Рашит </title> </head>
-<body>
 <?php
 $conn_string = "host=ec2-63-32-248-14.eu-west-1.compute.amazonaws.com port=5432 dbname=d4g919fr15hiaf user=hldnnyytoneyba password=715cf9b0dcfd8629ae613b113a1ba08f88dc46e0c94d649f353382f652617489 options='--client_encoding=UTF8'";
-$link = pg_connect($conn_string);
+$link = pg_connect($conn_string); //Подключение к БД
 
- $query = 'SELECT * FROM messag';
-$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
-while ($row=pg_fetch_array($result)){// для каждой строки из запроса
- echo "<tr>";
- echo "<td>" . $row['id'] . "</td>";
- echo "<td>" . $row['mail'] . "</td>";
- echo "<td>" . $row['mess'] . "</td>";
- echo "<td>" . $row['name'] . "</td>";
-  echo "</tr>";
+$query = 'SELECT * FROM messag'; //Создание запроса на получение всех записей из таблицы
+$result = pg_query($link,$query) or die('Ошибка запроса: ' . pg_last_error());
+while ($row=pg_fetch_row($result)){ // Для каждой строки из запроса
+	$mass = array_merge($mass,$row);
 }
 
+echo json_encode($mass,JSON_UNESCAPED_UNICODE);
 ?>
-
-</body> </html>
---
+++ setdata_3b.php
@@ -0,0 +1,20 @@
+<?php
+$conn_string = "host=ec2-63-32-248-14.eu-west-1.compute.amazonaws.com port=5432 dbname=d4g919fr15hiaf user=hldnnyytoneyba password=715cf9b0dcfd8629ae613b113a1ba08f88dc46e0c94d649f353382f652617489 options='--client_encoding=UTF8'";
+$link = pg_connect($conn_string); //Подключение к БД
+
+$mail = $_GET["mail"]; //Получение введенных данных
+$mess = $_GET["mess"];
+$name = $_GET["name"];
+
+$query="insert into messag (name,mail,mess) 
+values ('".$name."','".$mail."','".$mess."')"; //Создание запроса для БД
+
+$result = "Произошла ошибка."; //Сообщение об ошибке по умолчанию
+
+pg_query($link,$query);
+if (mysqli_affected_rows($link)>0) 
+{
+	$result = "Отзыв успешно создан!"; //При успешном выыполнении запроса результат меняется.
+}
+echo json_encode($result,JSON_UNESCAPED_UNICODE);
+?>
