82ed4338acc654682d7b1a5f123b385371f7865e
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2022-04-15 17:36:27 +0500

    проверка вывода данных

--- getdata_3b.php
+++ getdata_3b.php
@@ -1,12 +1,21 @@
+<html>
+<head> <title> Салихов Рашит </title> </head>
+<body>
 <?php
 $conn_string = "host=ec2-63-32-248-14.eu-west-1.compute.amazonaws.com port=5432 dbname=d4g919fr15hiaf user=hldnnyytoneyba password=715cf9b0dcfd8629ae613b113a1ba08f88dc46e0c94d649f353382f652617489 options='--client_encoding=UTF8'";
 $link = pg_connect($conn_string);
 
  $query = 'SELECT * FROM messag';
 $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
-echo "<tr>";
- echo "<td>" . $result['id'] . "</td>";
- echo "<td>" . $result['mail'] . "</td>";
- echo "<td>" . $result['mess'] . "</td>";
- echo "<td>" . $result['name'] . "</td>";
+while ($row=pg_fetch_array($result)){// для каждой строки из запроса
+ echo "<tr>";
+ echo "<td>" . $row['id'] . "</td>";
+ echo "<td>" . $row['mail'] . "</td>";
+ echo "<td>" . $row['mess'] . "</td>";
+ echo "<td>" . $row['name'] . "</td>";
+  echo "</tr>";
+}
+
 ?>
+
+</body> </html>
