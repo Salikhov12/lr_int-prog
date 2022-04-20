cc78440171fc5c3e58b968cb78f9deb5219d1812
Салихов Рашит <38361948+Salikhov12@users.noreply.github.com>
2022-04-20 18:20:24 +0500

    Отсортировал файлы ЛР, добавил начальный файл 4 ЛР
    Были добавлены библиотеки FancyZoom, jQuery-UI и jCarousel

--- index.php
+++ index.php
@@ -3,23 +3,26 @@
 </head>
 <ul>
 Лабораторная работа №1
-<li><a href="Lab1.html">1</a>
-<li><a href="Lab2.html">2</a>
-<li><a href="Lab3.html">3</a>
-<li><a href="Lab4.html">4</a>
-<li><a href="Lab5.html">5</a>
-<li><a href="Lab6.html">6</a>
-<li><a href="Lab7.html">7</a>
+<li><a href="LR Web-tech/1/Lab1.html">Задание 1</a>
+<li><a href="LR Web-tech/1/Lab2.html">Задание 2</a>
+<li><a href="LR Web-tech/1/Lab3.html">Задание 3</a>
+<li><a href="LR Web-tech/1/Lab4.html">Задание 4</a>
+<li><a href="LR Web-tech/1/Lab5.html">Задание 5</a>
+<li><a href="LR Web-tech/1/Lab6.html">Задание 6</a>
+<li><a href="LR Web-tech/1/Lab7.html">Задание 7</a>
 <br>Лабораторная работа №2
-<li><a href="lab2_pr.html">Примеры</a>
-<li><a href="lab_2-1.html">1</a>
-<li><a href="lab_2-2.html">2</a>
-<li><a href="lab_2-3.html">3</a>
-<li><a href="lab_2-4.html">4</a>
-<li><a href="lab_2-5.html">5</a>
-<li><a href="lab_2-6.html">6</a>
-<li><a href="lab_2-7.html">7</a>
+<li><a href="LR Web-tech/2/lab2_pr.html">Примеры</a>
+<li><a href="LR Web-tech/2/lab_2-1.html">Задание 1</a>
+<li><a href="LR Web-tech/2/lab_2-2.html">Задание 2</a>
+<li><a href="LR Web-tech/2/lab_2-3.html">Задание 3</a>
+<li><a href="LR Web-tech/2/lab_2-4.html">Задание 4</a>
+<li><a href="LR Web-tech/2/lab_2-5.html">Задание 5</a>
+<li><a href="LR Web-tech/2/lab_2-6.html">Задание 6</a>
+<li><a href="LR Web-tech/2/lab_2-7.html">Задание 7</a>
 <br>Лабораторная работа №3
-<li><a href="3.html">1</a>
-<li><a href="3b.html">2</a>
+<li><a href="LR Web-tech/3/3.html">Задание 1</a>
+<li><a href="LR Web-tech/3/3b.html">Задание 2</a>
+<br>Лабораторная работа №4
+<li><a href="LR Web-tech/4/lab4_pr.html">Примеры</a>
+<li><a href="LR Web-tech/4/Lab4.html">Задание 1</a>
 </ul>
